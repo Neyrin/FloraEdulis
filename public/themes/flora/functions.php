397ed5bdb74470a7a830cb6912afc2e4f9167ade
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-05-05 11:41:22 +0200

    Add soil support

--- public/themes/flora/functions.php
+++ public/themes/flora/functions.php
@@ -10,6 +10,14 @@ add_action('after_setup_theme', function () {
     // Disable the admin toolbar.
     show_admin_bar(false);
 
+    // Add soil support
+    add_theme_support('soil-clean-up');
+    add_theme_support('soil-disable-asset-versioning');
+    add_theme_support('soil-disable-trackbacks');
+    add_theme_support('soil-js-to-footer');
+    add_theme_support('soil-nice-search');
+    add_theme_support('soil-relative-urls');
+
     // Add post thumbnails support.
     add_theme_support('post-thumbnails');
 
