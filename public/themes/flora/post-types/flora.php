f0f9844b6686d9121a1506e2b27209c5fec0ee1f
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-05-05 10:40:57 +0200

    Create CPT for plants

---
+++ public/themes/flora/post-types/flora.php
@@ -0,0 +1,24 @@
+<?php 
+declare(strict_types=1);
+
+add_action('init', function () {
+    register_post_type('plant', [
+        'has_archive' => true,
+        'labels' => [
+            'add_new_item' => __('Add New Plant'),
+            'edit_item' => __('Edit Plant'),
+            'name' => __('Plants'),
+            'search_items' => __('Search Plants'),
+            'singular_name' => __('Plant'),
+        ],
+				'supports' => [
+            'title',
+            'editor',
+            'thumbnail',
+            'excerpt',
+        ],
+        'menu_icon' => 'dashicons-',
+        'menu_position' => 20,
+        'public' => true,
+    ]);
+});
