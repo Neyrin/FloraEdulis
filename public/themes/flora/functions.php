445a638cca0350d6e612ca705e43b08a67163517
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-05-05 12:00:13 +0200

    Enqueue stylesheet

--- public/themes/flora/functions.php
+++ public/themes/flora/functions.php
@@ -10,6 +10,9 @@ add_action('after_setup_theme', function () {
     // Disable the admin toolbar.
     show_admin_bar(false);
 
+    // Add post thumbnails support.
+    add_theme_support('post-thumbnails');
+
     // Add soil support
     add_theme_support('soil-clean-up');
     add_theme_support('soil-disable-asset-versioning');
@@ -17,9 +20,6 @@ add_action('after_setup_theme', function () {
     add_theme_support('soil-js-to-footer');
     add_theme_support('soil-nice-search');
     add_theme_support('soil-relative-urls');
-
-    // Add post thumbnails support.
-    add_theme_support('post-thumbnails');
 
     // Add title tag theme support.
     add_theme_support('title-tag');
@@ -43,11 +43,11 @@ add_action('after_setup_theme', function () {
 // Register flora custom post type.
 require get_template_directory().'/post-types/flora.php';
 
-// Register taxonomies for the flora custom post type.
+// Register taxonomies
 require get_template_directory().'/taxonomies/plant_categories.php';
 require get_template_directory().'/taxonomies/plant_flavours.php';
 
-// Register ac-fields 
+// Register ac-fields
 require get_template_directory().'/custom-fields/plants-acf.php';
 
 // Enqueue and register scripts the right way.
@@ -58,9 +58,12 @@ add_action('wp_enqueue_scripts', function () {
 
     // wp_register_script('wordplate', mix('scripts/app.js'), '', '', true);
     // wp_enqueue_script('wordplate');
+    wp_enqueue_style('style', get_stylesheet_uri());
 });
 
 // Remove JPEG compression.
 add_filter('jpeg_quality', function () {
     return 100;
 }, 10, 2);
+
+
