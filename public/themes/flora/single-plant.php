ab1773e48327d12329be523da2aef98be47eef76
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-05-10 19:11:27 +0200

    Update ACF-imports to simplify css

--- public/themes/flora/single-plant.php
+++ public/themes/flora/single-plant.php
@@ -1,29 +1,30 @@
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="col">
-        <?php if (have_posts()) : ?>
+  <div class="col">
+    <?php if (have_posts()) : ?>
 
-            <?php while (have_posts()): the_post(); ?>
+      <?php while (have_posts()): the_post(); ?>
+        <div class="img-container">
+            <img class="plant-img" src="<?php the_field('plant_photo'); ?>">
+        </div>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
 
-                <img src="<?php the_field('plant_photo'); ?>">
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
+        <p class="author"><b>Named by: </b><?php the_field('author'); ?></p>
+        <div class="color-info">
+          <h4>Color: </h4>
+          <div class="plant-color" style="background-color:<?php the_field('plant_color'); ?>"></div>
+        </div>
+        <p class="latin-name"><b>Latin name: </b><?php the_field('latin_name'); ?></p>
 
-                <h4>Named by: <?php the_field('author'); ?></h4>
-                <div class="color-info">    
-                    <h4>Color: </h4>
-                    <div class="plant-color" style="background-color:<?php the_field('plant_color'); ?>"></div>
-                </div>
-                <h4>Latin name: <?php the_field('latin_name'); ?></h4>
+        <?php previous_post_link(); ?>
 
-                <?php previous_post_link(); ?>
+        <?php next_post_link(); ?>
+      <?php endwhile; ?>
 
-                <?php next_post_link(); ?>
-            <?php endwhile; ?>
-
-        <?php endif; ?>
-    </div><!-- /col -->
+    <?php endif; ?>
+  </div><!-- /col -->
 </div><!-- /row -->
 
 <?php get_footer(); ?>
