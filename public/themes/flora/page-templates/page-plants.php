88fa3f69c723b096eb9dfab92e35f23f3ecf8ae3
Isa Areschoug <isaar@Isas-MacBook-Air.local>
2019-05-10 19:09:20 +0200

    Add image, clean up syntax

--- public/themes/flora/page-templates/page-plants.php
+++ public/themes/flora/page-templates/page-plants.php
@@ -6,40 +6,33 @@
 get_header(); ?>
 
 <div class="row mt-5">
-    <div class="col">
-
-      <?php if (have_posts()): ?>
-          
-          <?php while (have_posts()): the_post(); ?>
-    
-              <h1><?php the_title(); ?></h1>
-
-              <?php the_content(); ?>
-
-          <?php endwhile; ?>
-
-      <?php endif; ?>
-    
-    </div><!-- /col -->
+  <div>
+    <?php if (have_posts()): ?>
+      <?php while (have_posts()): the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div><!-- /col -->
 </div><!-- /row -->
 
 <?php $plants = get_posts(['post_type' => 'plant']); ?>
 
 <?php if (count($plants)): ?>
-    <div class="row mt-5">
-        <div class="col">
-            <ul>
-                <?php foreach ($plants as $plant): ?>
-                    <li class="mb-2">
-                        <a href="<?php the_permalink($plant); ?>"><h2><?php echo $plant->post_title; ?></h2></a>
-                        <p><?php echo $plant->post_excerpt; ?></p>
-                        <a href="<?php the_permalink($plant); ?>" class="button">To the article</a>
-                        
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div><!-- /col -->
-    </div><!-- /row -->
+  <div class="row mt-5">
+    <div class="col">
+      <ul>
+        <?php foreach ($plants as $plant): ?>
+          <li class="mb-2">
+            <a href="<?php the_permalink($plant); ?>"><h2><?php echo $plant->post_title; ?></h2></a>
+            <img src="<?php the_field('plant_photo'); ?>">
+            <p><?php echo $plant->post_excerpt; ?></p>
+            <a href="<?php the_permalink($plant); ?>" class="button">To the article</a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div><!-- /col -->
+  </div><!-- /row -->
 <?php endif; ?>
 
 <?php get_footer(); ?>
